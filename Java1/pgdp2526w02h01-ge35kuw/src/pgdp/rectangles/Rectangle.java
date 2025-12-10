e9c25181d228ab39726a7128c906c070d4ad428f
Hajdudavid47 <101344750+Hajdudavid47@users.noreply.github.com>
2025-12-10 12:48:52 +0100

    Add files via upload

---
+++ Java1/pgdp2526w02h01-ge35kuw/src/pgdp/rectangles/Rectangle.java
@@ -0,0 +1,21 @@
+package pgdp.rectangles;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		/*
+		 * You can use the main-method to test your code.
+		 * It is irrelevant to the grading as long as it compiles.
+		 */
+//		Vector2D tl = new Vector2D(0.0, 2.0), br = new Vector2D(2.0, 0.5);
+//		Rectangle myRectangle = new Rectangle(tl, br);
+//		if (myRectangle.toString().equals("Rectangle spanned by points [0.0, 2.0] and [2.0, 0.5]."))
+//			System.out.println("The String seems to match. Good Job!");
+//		else {
+//			System.out.println("This is what you created: " + myRectangle);
+//			System.out.println("This is how it should be: Rectangle spanned by points [0.0, 2.0] and [2.0, 0.5].");
+//			System.out.println("Better luck next time!");
+//		}
+	}
+
+}
---
+++ Java1/pgdp2526w02h01-ge35kuw/src/pgdp/rectangles/Vector2D.java
@@ -0,0 +1,20 @@
+package pgdp.rectangles;
+
+public class Vector2D {
+
+	public static void main(String[] args) {
+        /*
+         * You can use the main-method to test your code.
+         * It is irrelevant to the grading as long as it compiles.
+         */
+//		Vector2D myVector = new Vector2D(1.0, 2.0);
+//		if (myVector.toString().equals("[1.0, 2.0]"))
+//			System.out.println("The String seems to match. Good Job!");
+//		else {
+//			System.out.println("This is what you created: " + myVector);
+//			System.out.println("This is how it should be: [1.0, 2.0]");
+//			System.out.println("Better luck next time!");
+//		}
+	}
+
+}
